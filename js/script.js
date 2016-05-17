79694b3c3d050f55ec1b20a5e6e648eb27494227
AirSeeker <zimorget@gmail.com>
2016-05-17 15:06:17 +0300

    add/edit --done

--- js/script.js
+++ js/script.js
@@ -1,62 +1,134 @@
-
-
-
 var recivedData, addedData;
-var jqXHR = $.getJSON('https://api.myjson.com/bins/3uoya');
- jQuery.support.cors = true;
-jqXHR.complete(function(response) {
-	recivedData = response.responseJSON;
-	var header = $('h1').text();
-	$('h1').html(header+ "</br><i> status: "+ Object.keys(recivedData).length +" company's loaded</i>");
-	var output = '<ul>';
-	for (var key in recivedData) {
-		output += '<li>' + recivedData[key].name + " | " + recivedData[key].money + "K$"+ '</li>';
-		}
-		output += '</ul>';
-	$("#info").html(output);
-});
 
 
-$.ajax({
-    url:"https://api.myjson.com/bins/3uoya",
-    type:"PUT",
-    data:'{"key_updated":"value_updated"}',
-    contentType:"application/json; charset=utf-8",
-    dataType:"json",
-    success: function(data, textStatus, jqXHR){
-    	alert("done!");
-    }
-});  
+///////////////////////////////////////////////////////////////////////////////
+/////////////////////////POST && REVIEW////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+function postdata() {
+	$.ajax({
+	    url:"https://api.myjson.com/bins/3uoya",
+	    type:"PUT",
+	    data: JSON.stringify(addedData),
+	    contentType:"application/json; charset=utf-8",
+	    dataType:"json",
+	    success: function(data, textStatus, jqXHR){
+	    	alert("done!");
+	    	reviewdata();
+	    }
+	});
+};
 
-
-
-
-$('#view').click(function () {
-	$ajaxUtils.sendGetRequest('/data/data.json', function (res) {
-		var message = res.name + " " + res.money + " " + res.parent + " " + res.child + " " + Object.keys(res).length;
+function reviewdata() {
+	var jqXHR = $.getJSON('https://api.myjson.com/bins/3uoya');
+ 	jQuery.support.cors = true;
+	jqXHR.complete(function(response) {
+		recivedData = response.responseJSON;
+		addedData = recivedData;
+		$('h1').html("Company organizer"+ "</br><i> status: "+ Object.keys(recivedData).length +" company's loaded</i>");
 		var output = '<ul>';
-		for (var key in res) {
-			output += '<li>' + res[key].name + '</li>';
+		var list = '<option data-id="">none</option>';
+		for (var key in recivedData) {
+			list += '<option data-index="'+ key +'" data-id="'+recivedData[key].id+'">' + recivedData[key].name + '</option>';
+			output += '<li data-id="'+recivedData[key].id+'" data-money="'+recivedData[key].money+'">' + recivedData[key].name + " | " + recivedData[key].money + "K$"+ '</li>';
 		}
 		output += '</ul>';
 		$("#info").html(output);
+		$('.parent, .child, .company').html(list);
 	});
-});
+};
 
-$('#add').click(function () {
-	console.log(recivedData);
-});
-
+///////////////////////////////////////////////////////////////////////////////
+/////////////////////////EDIT//////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 $('#edit').click(function () {
-	$ajaxUtils.sendGetRequest('/data/data.json', function (res) {
-		var message = res.name + " " + res.money + " " + res.parent + " " + res.child + " " + Object.keys(res).length;
-		$("#info").text(message);
-	});
+	$('.here').append('<label>Pick company</label><div class="form-group"><select class="form-control company"></select></div>');
+	reviewdata();
 });
 
-$('#delete').click(function () {
-	$ajaxUtils.sendGetRequest('/data/data.json', function (res) {
-		var message = res.name + " " + res.money + " " + res.parent + " " + res.child + " " + Object.keys(res).length;
-		$("#info").text(message);
-	});
+$('.here').click(function () {
+	var companyIndex = $('.company option:selected').data("index");
+	var companyObject = recivedData[companyIndex];
+	var name = companyObject.name;
+	$('#company_name').val(name);
+	var money = companyObject.money;
+	$('#company_money').val(money);
+	var parent = companyObject.parent;
+	$(".parent option[data-id="+parent+"]").prop('selected', true);
+	var child = companyObject.child; 
+	$(".child option[data-id="+child+"]").prop('selected', true);
 });
+
+///////////////////////////////////////////////////////////////////////////////
+/////////////////////////DELETE////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+$('#delete').click(function () {
+	var company = $('.company option:selected').data("id");
+	console.log(company);
+});
+
+///////////////////////////////////////////////////////////////////////////////
+/////////////////////////DELETE////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+$('.submit').click(function () {
+	var freeId = [];
+	for(var i=0; i<recivedData.length; i++){
+		freeId.push(recivedData[i].id);
+	}
+	freeId.sort(function(a, b){return a-b;});
+	freeId=freeId[freeId.length-1]+1;
+	console.log(freeId);
+	var name = $('#company_name').val();
+	var money = $('#company_money').val();
+	var parent = $('.parent option:selected').data("id");
+	var child = $('.child option:selected').data("id");
+	var template = {
+		"id": freeId,
+		"name": name,
+		"money": money,
+		"parent": parent,
+		"child" : child
+	};
+	console.log(template);
+	addedData.push(template);
+	console.log(addedData);
+	postdata();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+reviewdata();
