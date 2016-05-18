c827d040166734443a144f33e833c053cf6fcbf7
AirSeeker <zimorget@gmail.com>
2016-05-18 12:44:54 +0300

    treeBuilder

--- js/script.js
+++ js/script.js
@@ -13,7 +13,25 @@ function postdata() {
 	    contentType:"application/json; charset=utf-8",
 	    dataType:"json",
 	    success: function(data, textStatus, jqXHR){
-	    	alert("done!");
+		    $(function(){
+					new PNotify({
+					    title: 'Success',
+					    text: 'Changes are applied!',
+					    type: 'success'
+					});
+				});
+	    	reviewdata();
+	    	$('input').val('');
+	    	$('option[data-id="none"]').prop('selected', true);
+	    },
+	    error: function(data, textStatus, jqXHR){
+		    $(function(){
+					new PNotify({
+				    title: 'Oh No!',
+				    text: 'Something terrible happened.',
+				    type: 'error'
+					});
+				});
 	    	reviewdata();
 	    }
 	});
@@ -25,16 +43,14 @@ function reviewdata() {
 	jqXHR.complete(function(response) {
 		recivedData = response.responseJSON;
 		addedData = recivedData;
-		$('h1').html("Company organizer"+ "</br><i> status: "+ Object.keys(recivedData).length +" company's loaded</i>");
-		var output = '<ul>';
-		var list = '<option data-id="">none</option>';
-		for (var key in recivedData) {
-			list += '<option data-index="'+ key +'" data-id="'+recivedData[key].id+'">' + recivedData[key].name + '</option>';
-			output += '<li data-id="'+recivedData[key].id+'" data-money="'+recivedData[key].money+'">' + recivedData[key].name + " | " + recivedData[key].money + "K$"+ '</li>';
+		addedData = sortByKey(addedData,'parent');
+		$('h1').html("<b>Company organizer</b></br><i> status: "+ Object.keys(addedData).length +" company's loaded</i>");
+		var list = '<option data-id="none">none</option>';
+		for (var key in addedData) {
+			list += '<option data-index="'+ key +'" data-id="'+addedData[key].id+'">' + addedData[key].name + '</option>';
 		}
-		output += '</ul>';
-		$("#info").html(output);
-		$('.parent, .child, .company').html(list);
+		$('.parent, .company').html(list);
+		treeBuilder();
 	});
 };
 
@@ -45,52 +61,56 @@ function reviewdata() {
 
 var dad, companyIndex;
 $('.picker').click(function () {
+	$('#left_side > div:nth-child(4) > select').html($('#'+dad+' .company').html());
 	dad = $(this).parent().parent().parent().parent().attr('id');
 	companyIndex = $('#'+dad+' .company option:selected').data('index');
-	var companyObject = recivedData[companyIndex];
+	var companyObject = addedData[companyIndex];
 	var name = companyObject.name;
 	$('#'+dad+' #company_name').val(name);
 	var money = companyObject.money;
 	$('#'+dad+' #company_money').val(money);
 	var parent = companyObject.parent;
 	$('#'+dad+' .parent option[data-id="'+parent+'"]').prop('selected', true);
-	var child = companyObject.child; 
-	$('#'+dad+' .child option[data-id="'+child+'"]').prop('selected', true);
+	$('#'+dad+' .parent option[data-id="'+companyObject.id+'"]').remove();
 });
 
 $('.submit_edit').click(function () {
-	//slice before submit
-	console.log(addedData[companyIndex]);
-	addedData.splice(companyIndex, 1);
-	console.log(addedData);
-	var freeId = [];
-	for(var i=0; i<addedData.length; i++){
-		freeId.push(addedData[i].id);
+	if($('#page2 #company_name').val() === "" || $('#page2 #company_money').val() === ""){
+		$(function(){
+			new PNotify({
+				title: 'Oh No!',
+				text: 'Please fill up input field\'s.',
+				type: 'error'
+			});
+		});
+	}else{
+		var name = $('#'+dad+' #company_name').val();
+		var money = $('#'+dad+' #company_money').val();
+		var parent = $('#'+dad+' .parent option:selected').data("id");
+		addedData[companyIndex].name = name;
+		addedData[companyIndex].money = money;
+		addedData[companyIndex].parent = parent;
+		postdata();
 	}
-	freeId.sort(function(a, b){return a-b;});
-	freeId=freeId[freeId.length-1]+1;
-	var name = $('#'+dad+' #company_name').val();
-	var money = $('#'+dad+' #company_money').val();
-	var parent = $('#'+dad+' .parent option:selected').data("id");
-	var child = $('#'+dad+' .child option:selected').data("id");
-	var template = {
-		"id": freeId,
-		"name": name,
-		"money": money,
-		"parent": parent,
-		"child" : child
-	};
-	addedData.push(template);
-	postdata();
 });
 ///////////////////////////////////////////////////////////////////////////////
 /////////////////////////DELETE////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 $('.submit_delete').click(function () {
-	var companyIndex = $('#page3 .company option:selected').data('index');
-	addedData.splice(companyIndex, 1);
-	postdata();
+	if($('#page3 .company option:selected').data("id") === "none"){
+		$(function(){
+			new PNotify({
+				title: 'Oh No!',
+				text: 'Please chose company for delete.',
+				type: 'error'
+			});
+		});
+	}else{
+		var companyIndex = $('#page3 .company option:selected').data('index');
+		addedData.splice(companyIndex, 1);
+		postdata();
+	}
 });
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -98,59 +118,104 @@ $('.submit_delete').click(function () {
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 $('.submit_add').click(function () {
-	var freeId = [];
-	for(var i=0; i<addedData.length; i++){
-		freeId.push(addedData[i].id);
+	if($('#page1 #company_name').val() === "" || $('#page1 #company_money').val() === ""){
+		$(function(){
+			new PNotify({
+				title: 'Oh No!',
+				text: 'Please fill up input field\'s.',
+				type: 'error'
+			});
+		});
+	}else{
+
+	for (var key in addedData) {
+		if(addedData[key].name === $('#company_name').val()){
+			return $(function(){
+				new PNotify({
+					title: 'Oh No!',
+					text: 'That name already exist! Change name please.',
+					type: 'error'
+				});
+			});
+		}
 	}
-	freeId.sort(function(a, b){return a-b;});
-	freeId=freeId[freeId.length-1]+1;
-	console.log(freeId);
-	var name = $('#company_name').val();
-	var money = $('#company_money').val();
-	var parent = $('.parent option:selected').data("id");
-	var child = $('.child option:selected').data("id");
-	var template = {
-		"id": freeId,
-		"name": name,
-		"money": money,
-		"parent": parent,
-		"child" : child
-	};
-	addedData.push(template);
-	postdata();
+		var freeId = [];
+		for(var i=0; i<addedData.length; i++){
+			freeId.push(addedData[i].id);
+		}
+		freeId.sort(function(a, b){return a-b;});
+		freeId=freeId[freeId.length-1]+1;
+		var name = $('#company_name').val();
+		var money = $('#company_money').val();
+		var parent = $('.parent option:selected').data("id");
+		var template = {
+			"id": freeId,
+			"name": name,
+			"money": money,
+			"parent": parent
+		};
+		addedData.push(template);
+		postdata();
+	}
 });
 
+///////////////////////////////////////////////////////////////////////////////
+/////////////////////////TREE BUILDER//////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+function treeBuilder() {
+	var output = '<ul>';
+	for (var key in addedData) {
+		var status = hasChild(addedData[key].id, addedData[key].parent);
+		// if(status === false){
+		// 	<ul data-index="" data-id=""></ul>
+		// }
 
 
 
 
+		output += '<li data-index="'+ key +'" data-id="'+addedData[key].id+'" data-money="'+addedData[key].money+'">' + addedData[key].name + " | " + addedData[key].money + "K$"+ '</li>';
+		console.log(addedData[key].parent);
 
+		console.log("Company name: "+ addedData[key].name +" hasChild: " + status);
+	}
 
+		output += '</ul>';
+		$("#tree").html(output);
+}
 
+function hasChild(id, parent) {
+	var template = "";
+	if(parent === "none"){
+		for (var key in addedData) {
+			console.log(id);
+			if(addedData[key].parent === id){
+				template += '<li data-index="'+key+'" data-id="'+addedData[key].id+'" data-money="'+addedData[key].money+'">'+ addedData[key].name +'</li>';
+			}
+		}
+		console.log(template);
+		return false;
+	}else if (id > 0) {
+		return true;
+	} else {
+		return false;
+	}
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+///////////////////////////////////////////////////////////////////////////////
+/////////////////////////SORT//////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+function sortByKey(array, key) {
+	return array.sort(function(a, b) {
+	  var x = a[key]; var y = b[key];
+	  return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+	}).reverse();
+};
 
 
 
 
 reviewdata();
+// TO DO
+// finish treeBuilder
